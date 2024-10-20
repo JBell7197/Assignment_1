16de17eb2ef18299a355441f17a34959ec06ac09
Justin <justinbell0701@outlook.com>
2024-10-20 12:27:32 -0500

    Initial Commit

---
+++ Assignment 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 1
+//
+//  Created by Justin Bell on 10/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
